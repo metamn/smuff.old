86968ebd14217b8e0bda16387a94a8523140b099
rubycs <rubycs@clair.ro>
2010-04-08 00:36:26 +0300

    News & sticky moved to sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,15 @@
 <?php 
   if (is_front_page()) {
     $sidebar_posts = query_posts2('posts_per_page=5&cat=47');
+    $news_posts = query_posts2('posts_per_page=3&cat=17');
+    
+    $sticky_posts = query_posts2('posts_per_page=1&cat=103');
+    if ($sticky_posts->have_posts()) {
+      while ($sticky_posts->have_posts()) : $sticky_posts->the_post(); update_post_caches($posts); 
+        $sticky_post = $post;
+        break;            
+      endwhile; 
+    }       
   } else {
     $cat = category_id(true, false);
     $sidebar_posts = query_posts2('posts_per_page=5&cat=' . $cat);
@@ -9,37 +18,6 @@
 
 <div id="sidebar" class="column span-6 last">
     <?php include "home-menu.php" ?>
-    
-    <?php if (is_single()) { ?>
-      <div id="new-items" class="box">
-        <h2>Produse <span class="highlight-title">similare</span></h2>            
-      </div>
-      <div class="arrow"><div class="arrow-bottom"></div></div>
-      <div id="new-items-list" class="box">  
-        <?php 
-        $related_posts = MRP_get_related_posts($post->ID, true);
-        if ($related_posts) {
-          foreach ($related_posts as $post) {
-            setup_postdata($post);
-            include "home-sales-thumb.php";
-          }
-        }
-        ?>
-      </div>    
-    <?php } else { ?>
-      <div id="new-items" class="box">
-        <h2>Produse <span class="highlight-title">noi</span></h2>            
-      </div>
-      <div class="arrow"><div class="arrow-bottom"></div></div>
-      <div id="new-items-list" class="box">
-        <?php           
-          if ($sidebar_posts->have_posts()) {
-            while ($sidebar_posts->have_posts()) : $sidebar_posts->the_post(); update_post_caches($posts); 
-              include "home-sales-thumb.php";              
-            endwhile; 
-          }
-        ?>          
-      </div>
-          
-    <?php } ?>
+    <?php include "home-sticky.php" ?>
+    <?php include "home-news.php" ?>
 </div>
