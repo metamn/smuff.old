0f69acdb85214e6f259133e1545c1635c5591b8c
rubycs <rubycs@clair.ro>
2010-04-08 14:37:08 +0300

    S3slider styled

--- home-top-sold.php
+++ home-top-sold.php
@@ -1,17 +1,6 @@
 <div id="top-sales" class="block"> 
 
-  <div id="top-sales-title" class="column span-4 last"> 
-    <div class="block"> 
-      <div id="text" class="column span-3 last">
-        Hot!
-      </div>
-      <div class="column span-1 last arrow-vertical">
-        <div class="arrow-right"></div>
-      </div>  
-    </div>    
-  </div>
-
-   <div id="top-sales-content" class="column span-14 last">    
+  <div id="top-sales-content" class="block">    
     <div id="s3slider">
       <?php if ($top_sales) { ?>
         <ul id="s3sliderContent">
@@ -34,7 +23,18 @@
         </ul>
       <?php } ?>
     </div>   
-   </div>
+  </div>
+  
+  <div id="top-sales-title" class="block"> 
+    <div class="block"> 
+      <div id="text" class="column span-3 last">
+        Hot!
+      </div>
+      <div class="column span-1 last arrow-vertical">
+        <div class="arrow-right"></div>
+      </div>  
+    </div>    
+  </div>
 
 </div>
 
