2966640b1f6fa78030ac2843d4fd6a6db0752f6b
rubycs <rubycs@clair.ro>
2010-07-27 21:13:40 +0300

    Sidebar restyled to the new Hot style

--- searchform.php
+++ searchform.php
@@ -1,12 +1,10 @@
 <?php 
-  $search_text = " ";
+  $search_text = "expresia cautata";
 ?>
 <form method="get" id="searchform" action="<?php bloginfo('home'); ?>/"> 
-  <span class="ui-icon ui-icon-search"/></span><label>Cautare rapida</label>
-  <br/>
-  <input type="text" value="<?php echo $search_text; ?>" name="s" id="s"  onblur="if (this.value == '')  {this.value = '<?php echo $search_text; ?>';}"  onfocus="if (this.value == '<?php echo $search_text; ?>')  {this.value = '';}" /> 
+  <input type="text" value="<?php echo $search_text; ?>" name="s" id="s"  onfocus="if (this.value == '<?php echo $search_text; ?>')  {this.value = '';}" /> 
   <?php include "search_price.php" ?>
-  <input class="submit" type="submit" id="searchsubmit" value="Cautare dupa pret" />  
+  <input class="submit" type="submit" id="searchsubmit" value="Cautare" />  
 </form>
 
 
