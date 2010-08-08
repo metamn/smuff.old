403b44f73bb66934254ef600d1ef15fe5a4fbfdd
rubycs <rubycs@clair.ro>
2010-08-08 18:46:36 +0300

    Page styled, subpages added

--- page.php
+++ page.php
@@ -11,12 +11,21 @@ get_header(); ?>
 	  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class="post" id="post-<?php the_ID(); ?>">
 		  <h2><?php the_title(); ?></h2>
-			<div class="entry">
-				<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
-				<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+		  
+		  <?php if ( is_page('despre-noi') || $post->post_parent == 331 || in_array( 331, $post->ancestors) ) { ?>
+		    <ul class="inline-list">
+		      <?php wp_list_pages('child_of=331&title_li='); ?>
+		    </ul>
+			<?php } ?>
+			
+			<div class="entry column span-18">
+				<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>				
 			</div>
 		</div>
 		<?php endwhile; endif; ?>
+		
+		<?php if (comments_open()) {comments_template();}  ?>
+		
 	  <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
 	</div>    
 	
