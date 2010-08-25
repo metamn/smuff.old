7e7af25b38eb0022358a6792ae9dfb656e5dea89
rubycs <rubycs@clair.ro>
2010-08-25 15:17:17 +0300

    Datafeed / GoShopping Plugin scheleton ready

--- plugins/datafeed/datafeed.php
+++ plugins/datafeed/datafeed.php
@@ -33,33 +33,78 @@ License: GPL2
 
 add_action('admin_menu', 'my_plugin_menu');
 
+// Adding menus and submenus
 function my_plugin_menu() {
   add_menu_page('Datafeed', 'Datafeed', 'manage_options', 'datafeed-menu', 'datafeed_main_page' );
+  
   add_submenu_page('datafeed-menu', 'GoShopping', 'GoShopping', 'manage_options', 'datafeed-goshopping', 'goshopping_options');
+  add_action( 'admin_init', 'register_options' );
+}
+
+// Adding DB field in Options database
+function register_options() {
+  register_setting( 'datafeed', 'goshopping' );
 }
 
 
-
+// Main datafeed dashboard
 function datafeed_main_page() {
   if (!current_user_can('edit_posts'))  {
     wp_die( 'Nu aveti drepturi suficiente de acces.' );
   }
   
+  echo '<div class="wrap">';
   echo '<h2>Datafeed</h2>';
+  echo '</div>';
 }
 
 
+/* Goshopping
+*/
+
+// Main function on Admin screen
 function goshopping_options() {
 
   if (!current_user_can('edit_posts'))  {
     wp_die( 'Nu aveti drepturi suficiente de acces.' );
   }
-
+  
   echo '<div class="wrap">';
-  echo '<p>Here is where the form would go if I actually had options.</p>';
+  echo '<h2>GoShopping</h2>';
+  
+  if ($_POST) {
+    goshopping_process_form($_POST);
+  } else { 
+    goshopping_display_form();
+  }
+  
   echo '</div>';
-
 }
 
+
+// Precessing input
+function goshopping_process_form($data) {
+  echo 'processing ... ';
+  echo $data['goshopping'];
+}
+
+// Displaying a form
+function goshopping_display_form() {
+?>
+<form method="post" action="">
+    <?php settings_fields( 'datafeed' ); ?>
+    <input type="hidden" name="goshopping-processing" value="1">
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row">Articole</th>
+        <td><input type="text" name="goshopping" value="<?php echo get_option('goshopping'); ?>" /></td>
+        </tr>
+    </table>
+    
+    <p class="submit">
+      <input type="submit" class="button-primary" value="Run" />
+    </p>
+</form>
+<?php }
 
 ?>
