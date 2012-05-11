b9f1d81d79bee8219a7c500e6279123ed836c9e2
metamn <bartus.csongor@gmail.com>
2012-05-11 11:44:49 +0300

    Home improvement added

--- home-icons.php
+++ home-icons.php
@@ -6,13 +6,18 @@
     </li>
     
     <li>
-      <a title="Cadouri audio, video" href="<?php bloginfo('home')?>/tag/magazin-audio-video">
+      <a title="Cadouri audio video" href="<?php bloginfo('home')?>/tag/magazin-audio-video">
        <img title="Cadouri audio, video" src="<?php bloginfo('stylesheet_directory')?>/img/icon-audiovideo.png" /></a>      
     </li>
     
     <li>
       <a title="Cadouri sport si outdoor" href="<?php bloginfo('home')?>/tag/cadouri-sport">
        <img title="Cadouri sport si outdoor" src="<?php bloginfo('stylesheet_directory')?>/img/icon-sport.png" /></a>      
+    </li>
+    
+    <li>
+      <a title="Cadouri pentru comfortul de acasa" href="<?php bloginfo('home')?>/tag/magazin-comfort-pentru-acasa">
+       <img title="Cadouri pentru comfortul de acasa" src="<?php bloginfo('stylesheet_directory')?>/img/icon-homeimprovement.png" /></a>      
     </li>
   </ul>
 </div>
