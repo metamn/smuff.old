6652c318bc47d55ad80c226dc6d00c7f0b604fb2
metamn <bartus.csongor@gmail.com>
2012-06-17 12:31:10 +0300

    Giftshopper profile edit is ok

--- giftshopper-profile.php
+++ giftshopper-profile.php
@@ -1,4 +1,13 @@
-<h3>Cadoul perfect <span id="name"></span></h3>
+<?php
+  
+  $name = '';
+  if ($profile) {
+    $name = "pentru <span class='highlight'>$profile->name</span>";
+  }
+
+?>
+
+<h3>Cadoul perfect <span id="name"><?php echo $name?></span></h3>
 <dl>
   <?php 
       $counter = 1;
@@ -10,11 +19,37 @@
             } else {
               echo get_the_category_by_ID($c);
             } ?>
-          <dd>&mdash;</dd>
+          <dd>
+            <?php 
+              if ($categories) {
+                $childrens = get_categories("child_of=" . $c);
+                $children_ids = array();
+                foreach ($childrens as $children) {
+                  $children_ids[] = $children->term_id;
+                }
+                $output = "";                
+                foreach ($categories as $cat) {
+                  if (in_array($cat, $children_ids)) {
+                    $output .=  '<br/>' . get_the_category_by_ID($cat);
+                  }
+                }
+                if ($output == "") { $output = "---"; }
+                echo $output;
+              } else {
+                echo "---";
+              }
+            ?>
+          </dd>
         </dt>
       <?php
       $counter += 1; 
     } ?>
   <dt id="step-budget">Buget</dt>
-  <dd>&mdash;</dd>
+  <dd>
+    <?php if ($profile->price) {
+      echo $profile->price;
+    } else {
+      echo "---";
+    } ?>
+  </dd>
 </dl>
