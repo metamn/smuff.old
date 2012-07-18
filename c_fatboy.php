c287d584fb1fdd62a83c095dfedd6de5124ef4eb
metamn <bartus.csongor@gmail.com>
2012-07-18 08:35:10 +0300

    Another set of design tweaks fro DOW

--- c_fatboy.php
+++ c_fatboy.php
@@ -1,7 +1,5 @@
 <div id="fatboy" class="block">
   <a href="http://www.smuff.ro/despre-noi/marele-concurs-pentru-marele-fatboy/" title="Castiga un Fatboy original">
-    <img src="<?php bloginfo('stylesheet_directory')?>/img/xxsdow-fatboy.png" title="Castiga un Fatboy original" />
-  <h1>Cumpara de la Smuff si castigi!</h1>
-  <p>Ce este?</p>
+    <img src="<?php bloginfo('stylesheet_directory')?>/img/xxsdow-fatboy.png" title="Castiga un Fatboy original" />  
   </a>
 </div>
--- c_invite-a-friend.php
+++ c_invite-a-friend.php
@@ -2,11 +2,9 @@
   <h4>
     Invita un prieten la newsletter-ul Smuff si <strong>amandoi va inscrieti la concursul saptamanii</strong> pentru 
     <br/><br/>
-    <strong><?php echo $product_title ?></strong>
+    <strong class="inverted"><?php echo $product_title ?></strong>
   </h4>
   <input type="email" required="" placeholder="Adresa Dvs. de email" id="mce-EMAIL" value="" name="EMAIL" class="text">
   <?php include 'mailchimp.php'; ?>
 </div>
-<div id="stamp">
-  <img src="<?php bloginfo('stylesheet_directory')?>/img/dow-stamp.png">
-</div>
+
