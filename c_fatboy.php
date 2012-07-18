c89b51bc85f314a8faf1c61e2dd3de19bd7ac8c2
metamn <bartus.csongor@gmail.com>
2012-07-18 08:42:33 +0300

    Starting Invite A Friend

--- c_fatboy.php
+++ c_fatboy.php
@@ -1,5 +1,5 @@
 <div id="fatboy" class="block">
   <a href="http://www.smuff.ro/despre-noi/marele-concurs-pentru-marele-fatboy/" title="Castiga un Fatboy original">
-    <img src="<?php bloginfo('stylesheet_directory')?>/img/xxsdow-fatboy.png" title="Castiga un Fatboy original" />  
-  </a>
+      &nbsp;
+  </a>    
 </div>
