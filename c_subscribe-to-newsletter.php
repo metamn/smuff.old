6eaf5458685c3ca4e0f76c95bf495ae31b6af568
metamn <bartus.csongor@gmail.com>
2012-07-26 17:52:16 +0300

    subscribe to newsletter done

--- c_subscribe-to-newsletter.php
+++ c_subscribe-to-newsletter.php
@@ -1,10 +1,12 @@
 <div id="subscribe-newsletter" class="campaign-box">
-  <h4>
-    Inscrie-te la newsletter si ai o <strong>livrare gratuita!</strong>
-  </h4>
-  <?php include 'mailchimp.php'; ?>
-  
-  <h4 id="whatis">
-    <a href="" title="Newsletterul Smuff">Ce este?</a>
-  </h4>
+  <div id="arrow">
+    <img src="<?php bloginfo('stylesheet_directory')?>/img/dow-arrow.png" title="Inscriere la newsletterul Smuff">
+  </div>
+  <div id="text">
+    <h4>
+      Inscrie-te la newsletter si ai o <strong>livrare gratuita!</strong>
+    </h4>
+    <?php include 'mailchimp.php'; ?>  
+    <a id="whatis" href="" title="Newsletterul Smuff">Ce este?</a>  
+  </div>
 </div>
