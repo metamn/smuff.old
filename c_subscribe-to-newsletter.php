6843b9d69bd48cf9922f69537068b1c3993f22b0
Your Name Here <bartus.csongor@gmail.com>
2012-09-30 10:37:03 +0300

    Next to add facebook connect

--- c_subscribe-to-newsletter.php
+++ c_subscribe-to-newsletter.php
@@ -1,12 +1,14 @@
 <div id="subscribe-newsletter" class="campaign-box">
-  <div id="arrow">
-    <img src="<?php bloginfo('stylesheet_directory')?>/img/dow-arrow.png" title="Inscriere la newsletterul Smuff">
-  </div>
+  <div class="arrow-right"></div>
   <div id="text">
     <h4>
       Inscrie-te la newsletter si ai o <strong>livrare gratuita!</strong>
     </h4>
     <?php include 'mailchimp.php'; ?>  
     <a id="whatis" href="http://www.smuff.ro/despre-noi/inscriere-la-newsletter/" title="Newsletterul Smuff">Ce este?</a>  
+  </div>
+  <div id="facebook-connect">
+  	<h4>Sau</h4>
+  	Facebook connect
   </div>
 </div>
