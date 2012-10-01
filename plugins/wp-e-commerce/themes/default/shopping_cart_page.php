c74e2e62f8b22dd80cc686761639242173491ed0
Your Name Here <bartus.csongor@gmail.com>
2012-10-01 05:09:02 +0300

    FB Connect added to Checkout

--- plugins/wp-e-commerce/themes/default/shopping_cart_page.php
+++ plugins/wp-e-commerce/themes/default/shopping_cart_page.php
@@ -29,13 +29,14 @@
     endwhile;
 ?>
 
+<!--
 <div id="announcement" class="block">
   <h4>Am facut mici schimbari la designul siteului Smuff. 
   <br/>
   Va rugam apasati CTRL+R (Refresh) pentru o experienta mai placuta. 
   Va multumim.</h4> 
 </div>
-
+-->
 
 <table class="productcart">
 	<tr class="firstrow">
@@ -357,6 +358,7 @@
   
     <div id="form" class="column span-7 append-1">	    
 	    <h2 class="checkout">Shopping rapid <br/>in 10 secunde</h2>
+	    <div class="fb-login-button" scope="email">Conectare cu Facebook</div>
 	    <table class='wpsc_checkout_table'>
 		    <?php while (wpsc_have_checkout_items()) : wpsc_the_checkout_item(); ?>
 			    <?php if(wpsc_is_shipping_details()) : ?>
