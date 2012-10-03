9c777506ffa6534087825452a074502c06f0e0e6
Your Name Here <bartus.csongor@gmail.com>
2012-10-03 09:00:47 +0300

    Discontinued category added

--- single-for-post.php
+++ single-for-post.php
@@ -2,6 +2,22 @@
   <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
   <div class="entry span-16 last">
     <?php 
+    	if (in_category(2237)) { // 2221 in localhost ?>
+    		<div class="notice">
+    			<h3>Acest produs nu se mai vinde pe Smuff</h3>
+    		</div>
+    		<?php 
+    		
+    		// Discontinued product
+    		$imgs = post_attachements($post->ID);
+        foreach ($imgs as $img) {
+          $large = wp_get_attachment_image_src($img->ID, 'full'); ?>
+          <div class="item">
+            <img src="<?php echo $large[0]?>" title="<?php echo $title ?>" alt="<?php echo $title ?>"/>
+          </div><?php 
+        }          
+    	}
+    
     	the_content('<p class="serif">Detalii &rarr;</p>');
     	if (!is_single(4721)) {
     		comments_template();
