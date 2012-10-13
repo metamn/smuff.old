ddd2b7f6d7731453cb980c4b63e257a90f294ba8
Your Name Here <bartus.csongor@gmail.com>
2012-10-13 12:47:33 +0300

    Working on search

--- search-enhanced.php
+++ search-enhanced.php
@@ -2,10 +2,13 @@
 	<?php include 'searchform.php' ?>
 	
 	<div id="categories">
-		<h3>Toate categoriile</h3>
+		<h3>&nbsp;</h3>
+		<ul>
+      <?php wp_list_categories('title_li=&exclude=8,96,9,10,22, 714, 716, 715, 717, 711, 712, 713'); ?>     
+    </ul>
 	</div>
 	
 	<div id="tags">
-		<h3>Etichete</h3>
+		<?php include 'home-icons.php' ?>
 	</div>
 </div>
