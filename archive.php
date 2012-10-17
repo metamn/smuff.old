9d83b2b84fab6306a755158bbaa19b33193c81a3
Your Name Here <bartus.csongor@gmail.com>
2012-10-17 13:53:21 +0300

    Started to rewrite archives and remove many unused files

--- archive.php
+++ archive.php
@@ -1,31 +1,56 @@
 <?php get_header(); ?>
 
-<?php 
-  $view = '0';
-    
-  $params = str_replace("%5B%5D", "", $_SERVER['QUERY_STRING']);		
-	$subs = explode("&", $params);		  	
-	$tmp = explode("=", $subs[0]);  // for apache
-	#$tmp = explode("=", $subs[1]); // for nginx
-	$view = $tmp[1];
+<?php
+  $cat = category_id(true, false, null);
+  if ($cat != 0) {
+		$args = array (
+			'posts_per_page' => 8,
+			'category__and' => array(15, $cat)
+		);
+		$promo_posts = query_posts2($args);
+		
+		$args = array (
+			'posts_per_page' => 6,
+			'category__and' => array(715, $cat) /* 14 */
+		);
+		$top_sales = query_posts2($args);  
+	} else {
+		// Queries for tags
+		$tag = get_query_var('tag_id'); 
+		
+		$args = array (
+			'posts_per_page' => 8,
+			'category__and' => array(15),
+			'tag__and' => array($tag)
+		);
+		$promo_posts = query_posts2($args);
+		
+		$args = array (
+			'posts_per_page' => 6,
+			'category__and' => array(715),
+			'tag__and' => array($tag)
+		);
+		$top_sales = query_posts2($args);  
+	}
 	
-  switch ($view) {
-    case '1':
-      include "archive-shop.php";
-      break;
-    case '2':
-      include "archive-all-table.php";
-      break;    
-    case '3':
-      include "archive-all-grid.php";
-      break;
-    default:
-      if (is_blog()) {
-        include "archive-blog.php";
-      } else {
-        include "archive-shop.php";  
-      }      
-  }
+	$new_products = $wp_query;
 ?>
+
+<div id="home" class="block">  
+  <div id="content" class="column span-18">   
+    <?php if ($new_products->have_posts()) {
+        include "home-hot.php";
+      } else {
+        echo '<h2>&nbsp;</h2><h2>Inca nu sunt produse in aceasta categorie</h2>';
+      }?>     
+           
+    <?php if ($top_sales->have_posts()) { include "home-bestsellers.php"; } ?> 
+    <?php if ($promo_posts->have_posts()) { include "home-promo.php"; }  ?>
+  </div> 
+</div>
+
+<?php get_footer(); ?>
+
+
 
 
--- startpage.php
+++ startpage.php
@@ -20,25 +20,12 @@
     'category__not_in' => array(1694)
   ));
   
-  
   $top_sales = query_posts2('posts_per_page=6&cat=14');  /* 14 */
-  
-  
   $promo_posts = query_posts2('posts_per_page=8&cat=15');
-  
-  // deal of the week  
-  //$dow_posts = query_posts2('posts_per_page=1&cat=2135'); /* 2135, 2101 */
-  
-  // gift of the week  
-  //$gow_posts = query_posts2('posts_per_page=1&cat=2163'); /* 2135, 2101 */
   
   // - Special posts are put first on HOT
   $special_posts = query_posts2('posts_per_page=1&cat=1317');
-  
-  // - Collections are put in between Hot & Bestsellers
-  // - Collections are items who are filed under Stiri + Collections category
-  $collections = query_posts2( array( 'category__and' => array( 22, 1695 ) ) );
-?>
+ ?>
 
 
 <div id="home" class="block">
@@ -50,13 +37,9 @@
           echo '<h2>&nbsp;</h2><h2>Inca nu sunt produse in magazin.</h2>';
         }?>        
         
-      <?php if ($collections->have_posts()) { include "home-collections.php"; } ?>   
       <?php if ($top_sales->have_posts()) { include "home-bestsellers.php"; } ?> 
       <?php if ($promo_posts->have_posts()) { include "home-promo.php"; }  ?>
-
-      
     </div>  
-    <?php get_sidebar(); ?>
   </div>  
 </div>
 
