20df2381dbd94d9f5d03b6f683e5a0135e78dfe0
Your Name Here <bartus.csongor@gmail.com>
2012-10-27 12:53:31 +0300

    Keyword search fixed

--- functions.php
+++ functions.php
@@ -10,9 +10,11 @@ global $wplogger;
 
 
 
-// Custom search: for price and delivery
+// Custom search
 // -------------------------------------------------------------------------------------------------
 
+
+// Search for price and delivery
 function custom_search($where) {
 	global $wpdb;
 	global $price;
@@ -72,6 +74,36 @@ function custom_search($where) {
   return $where;
 }
 
+
+// Search SQL filter for matching against post title only.
+function __search_by_title_only( $search, &$wp_query )
+{
+    global $wpdb;
+
+    if ( empty( $search ) )
+        return $search; // skip processing - no search term in query
+
+    $q = $wp_query->query_vars;    
+    $n = ! empty( $q['exact'] ) ? '' : '%';
+
+    $search =
+    $searchand = '';
+
+    foreach ( (array) $q['search_terms'] as $term ) {
+        $term = esc_sql( like_escape( $term ) );
+        $search .= "{$searchand}($wpdb->posts.post_title LIKE '{$n}{$term}{$n}')";
+        $searchand = ' AND ';
+    }
+
+    if ( ! empty( $search ) ) {
+        $search = " AND ({$search}) ";
+        if ( ! is_user_logged_in() )
+            $search .= " AND ($wpdb->posts.post_password = '') ";
+    }
+
+    return $search;
+}
+add_filter( 'posts_search', '__search_by_title_only', 500, 2 );
 
 
 // Subscribe to Mailchimp / Newsletter
