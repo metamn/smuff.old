178f0fa3c6089b8a894eccf0d98bfd8e5983b7b4
Your Name Here <bartus.csongor@gmail.com>
2012-11-01 11:36:17 +0200

    Facebook like box added

--- footer.php
+++ footer.php
@@ -1,3 +1,19 @@
+<div id="facebook-like-box" class="block">
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=348406981918786";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+
+	<div class="fb-like-box" data-href="http://www.facebook.com/smuffgadget" data-width="950" data-height="550" data-show-faces="true" data-border-color="#cccccc" data-stream="false" data-header="false"></div>
+</div>
+
+
+
+
 <div id="home-tag-icons" class="block">
 	<h3>Top categorii noi pe Smuff</h3>
   <ul class="inline-list">
