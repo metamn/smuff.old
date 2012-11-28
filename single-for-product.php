173a7defe55ea34ede311f7d3695c420cd3d5e8e
Your Name Here <bartus.csongor@gmail.com>
2012-11-28 12:40:00 +0200

    Product page styled

--- single-for-product.php
+++ single-for-product.php
@@ -170,30 +170,32 @@
 	</div>
 				
 	<div id="shopping-incentives">
-		<h4>Preturi accesibile</h4>
-		<p>
-			La Smuff toate preturile sunt corecte.  
-		</p>
-		
-		<h4>Shopping rapid</h4>
-		<p>
-			Fara procedura de inregistrare, numai cu un singur click.            
-		</p>
-		
-		<h4>Plata la livrare</h4>
-		<p>
-			Plata ramburs cand aveti deja produsul in mana.
-		</p>
-		
-		<h4>10 zile drept de retur</h4>
-		<p>
-			Fara intrebari din partea noastra.
-		</p>
-		
-		<h4>Garantie minim 1 an</h4>
-		<p>
-			Service Express sau schimb cu un produs NOU.
-		</p>        
+		<div class="inner">
+			<h4>Preturi accesibile</h4>
+			<p>
+				La Smuff toate preturile sunt corecte.  
+			</p>
+			
+			<h4>Shopping rapid</h4>
+			<p>
+				Fara procedura de inregistrare, numai cu un singur click.            
+			</p>
+			
+			<h4>Plata la livrare</h4>
+			<p>
+				Plata ramburs cand aveti deja produsul in mana.
+			</p>
+			
+			<h4>10 zile drept de retur</h4>
+			<p>
+				Fara intrebari din partea noastra.
+			</p>
+			
+			<h4>Garantie minim 1 an</h4>
+			<p>
+				Service Express sau schimb cu un produs NOU.
+			</p>
+		</div>
 	</div>
 </section>
  
