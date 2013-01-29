fa1315c53820757dd3b91f150b2bc422ec584ea5
metamn <bartus.csongor@gmail.com>
2013-01-29 18:21:59 +0200

    Responsive breakpoints converted back to pixels from ems

--- header.php
+++ header.php
@@ -9,8 +9,8 @@
 		
 	
 		
-		<meta content="width=device-width, initial-scale=1.0" name="viewport">
 		
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		
 		<title>		  		  
 		  <?php 
