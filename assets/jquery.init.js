64fced29c125bbabf130777db32322a9b57e2b3b
metamn <bartus.csongor@gmail.com>
2013-01-31 21:09:24 +0200

    Header and startpage styling removed from theme

--- assets/jquery.init.js
+++ assets/jquery.init.js
@@ -69,7 +69,22 @@ $(document).ready(function() {
   
   
   
+  // The Grid
   
+  var horizontal = "";
+  for (i=0; i<400; i++) {
+    horizontal += '<div class="col">&nbsp;</div>';
+  }
+  
+  var vertical = ""
+  for (i=0; i<500; i++) {
+    vertical += '<div class="col">&nbsp;</div>';
+  }
+  
+  jQuery("body").append(
+    '<div id="gridh" class="grid">' + horizontal + '</div>' +
+    '<div id="gridv" class="grid">' + vertical + '</div>'
+  );
   
   
   
