86d814f91168ba7c0c0ea1a0c7b2799806cd6cdf
metamn <bartus.csongor@gmail.com>
2013-02-22 19:02:39 +0200

    Refactoring responsive layouts, working on mobile startpage

--- c_giftshopper.php
+++ c_giftshopper.php
@@ -4,19 +4,19 @@
   </header>
   
   <div id="body">
-    <ul>
-      <li id="for-him">
-        <p>Pentru El</p>
-      </li> 
-      <li id="for-her">
-        <p>Pentru Ea</p>
-      </li>
-      <li id="for-kids">
-        <p>Pentru Copii</p>
-      </li>
-      <li id="for-office" class="last">
-        <p>Casa si birou</p>
-      </li>
-    </ul>
+    
+    <div id="for-him" class="c1">
+      <p>Pentru El</p>
+    </div> 
+    <div id="for-her" class="c2">
+      <p>Pentru Ea</p>
+    </div>
+    <div id="for-kids" class="c3">
+      <p>Pentru Copii</p>
+    </div>
+    <div id="for-office" class="c4">
+      <p>Casa si birou</p>
+    </div>
+    
   </div>
 </section>
