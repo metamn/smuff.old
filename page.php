cad8a37eebf781f8b9f718944bdd26283fa2753d
metamn <bartus.csongor@gmail.com>
2013-03-14 12:00:14 +0200

    Page done

--- page.php
+++ page.php
@@ -26,76 +26,86 @@ get_header();
 	
 	<aside id="sidebar">
 	  <?php if (is_page('430')) { ?>
-	  <div id="account" class="tabs">
-	    <ul id="tabs">
-	      <li id="connected">Date de livrare</li>
-	      <li id="connect">Conectare</li>
-	    </ul>
-	    
-	    <div id="connected">
-	      <ul>
-	        <li>
-	          <input type="text" name="name" id="name" value="">
-	          <label>Nume si prenume *</label>
-	        </li>
-	        
-	        <li>
-	          <input type="text" name="email" id="email" value="">
-	          <label>Email *</label>
-	        </li>
-	        
-	        <li>
-	          <input type="text" name="phone" id="phone" value="">
-	          <label>Telefon *</label>
-	        </li>
-	        
-	        <li>
-	          <textarea name="address" id="address"></textarea>
-	          <label>Adresa *</label>
-	        </li>
-	        
-	        <li>
-	          <input type="text" name="city" id="city" value="">
-	          <label>Localitate si judet *</label>
-	        </li>
-	        
+	    <div id="account" class="tabs">
+	      <ul id="tabs">
+	        <li id="connected">Date de livrare</li>
+	        <li id="connect">Conectare</li>
 	      </ul>
+	      
+	      <div id="connected">
+	        <ul>
+	          <li>
+	            <input type="text" name="name" id="name" value="">
+	            <label>Nume si prenume *</label>
+	          </li>
+	          
+	          <li>
+	            <input type="text" name="email" id="email" value="">
+	            <label>Email *</label>
+	          </li>
+	          
+	          <li>
+	            <input type="text" name="phone" id="phone" value="">
+	            <label>Telefon *</label>
+	          </li>
+	          
+	          <li>
+	            <textarea name="address" id="address"></textarea>
+	            <label>Adresa *</label>
+	          </li>
+	          
+	          <li>
+	            <input type="text" name="city" id="city" value="">
+	            <label>Localitate si judet *</label>
+	          </li>
+	          
+	        </ul>
+	      </div>
+	      
+	      <div id="connect">
+	        <ul>
+	          <li id="facebook">
+	            <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/facebook-connect.jpg" />
+	          </li>
+	          
+	          <li id="or">
+	            sau
+	            <br/>
+	            Shopping Rapid
+	          </li>
+	          
+	          <li>
+	            <input type="text" name="name" id="name" value="">
+	            <label>Nume si prenume *</label>
+	          </li>
+	          
+	          <li>
+	            <input type="text" name="email" id="email" value="">
+	            <label>Email *</label>
+	          </li>
+	          
+	          <li>
+	            <input type="text" name="phone" id="phone" value="">
+	            <label>Telefon *</label>
+	          </li>
+	        </ul>
+	      </div>
+	     
 	    </div>
 	    
-	    <div id="connect">
+	    <div id="checkout">
+	      <input type='submit' value="Trimite comanda acum" />
+	    </div>
+	  <?php } else { ?>
+	    <div id="other-info">
+	      <h5>Alte informatii</h5>
 	      <ul>
-	        <li id="facebook">
-	          <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/facebook-connect.jpg" />
-	        </li>
-	        
-	        <li id="or">
-	          sau
-	          <br/>
-	          Shopping Rapid
-	        </li>
-	        
-	        <li>
-	          <input type="text" name="name" id="name" value="">
-	          <label>Nume si prenume *</label>
-	        </li>
-	        
-	        <li>
-	          <input type="text" name="email" id="email" value="">
-	          <label>Email *</label>
-	        </li>
-	        
-	        <li>
-	          <input type="text" name="phone" id="phone" value="">
-	          <label>Telefon *</label>
-	        </li>
+	        <li><a href="">Despre noi</a></li>
+	        <li><a href="">Cum cumpar?</a></li>
+	        <li><a href="">Contacteaza-ne</a></li>
+	        <li><a href="">Termeni si conditii</a></li>
 	      </ul>
 	    </div>
-	   
-	  </div>
-	  
-	  <div id="checkout">
-	    <input type='submit' value="Trimite comanda acum" />
-	  </div>
 	  <?php } ?>
 	</aside>
 	  
